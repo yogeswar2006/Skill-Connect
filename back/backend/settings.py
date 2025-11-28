c4c2ca88e0924ee9da45cf3b529b4728a61d3213
Yogi Reddy <yogi8247322760@gmail.com>
2025-11-28 13:44:56 +0530

    docker updated

--- back/backend/settings.py
+++ back/backend/settings.py
@@ -15,8 +15,8 @@ from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 load_dotenv(os.path.join(BASE_DIR,'.env'))
 
