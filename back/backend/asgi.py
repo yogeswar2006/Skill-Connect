e8a81441536104166d188c120ca505e9e6db8cac
yogeswar2006 <yogi8247322760@gmail.com>
2026-02-03 20:49:59 +0530

    updated asgi

--- back/backend/asgi.py
+++ back/backend/asgi.py
@@ -44,10 +44,12 @@
 
 import os
 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
-
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
@@ -56,7 +58,7 @@ from .middleware import JWTAuthMiddleware
 
 
 
-django_asgi_app = get_asgi_application()
+
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
