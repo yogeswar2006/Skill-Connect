e9f26107f26467f1ed2a801b4518ac49ecfa31a0
yogeswar2006 <yogi8247322760@gmail.com>
2026-02-04 14:19:19 +0530

    updated version

--- back/backend/settings.py
+++ back/backend/settings.py
@@ -212,6 +212,11 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+WHITENOISE_SKIP_COMPRESS_EXTENSIONS = [
+    "html",
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
